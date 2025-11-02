e6ae1e175653810f8e290f9ceac28176c0f8f132
Atta Ullah Noor <151811576+Noor27266@users.noreply.github.com>
2025-11-02 20:42:39 +0800

    Update RC Wall GUI Codes.py

--- RC Wall GUI Codes.py
+++ RC Wall GUI Codes.py
@@ -120,8 +120,8 @@ st.markdown("""
 <style>
 /* Design reference for "full" layout (adjust if you want). */
 :root{
-  --design-w: 1600;    /* width in px of your intended one-screen layout */
-  --design-h: 900;     /* height in px of your intended one-screen layout */
+  --design-w: 2300;    /* width in px of your intended one-screen layout */
+  --design-h: 1200;     /* height in px of your intended one-screen layout */
   --scale: min( calc(100vw / var(--design-w)), calc(100vh / var(--design-h)) );
 }
 
@@ -885,3 +885,4 @@ if show_recent and not st.session_state.results_df.empty:
 
 # === close fit-to-screen wrapper (added) ===
 st.markdown("</div>", unsafe_allow_html=True)
+
