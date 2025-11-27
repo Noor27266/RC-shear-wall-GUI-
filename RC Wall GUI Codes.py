e536a1aa9025f8f82a081428349c05621806c34d
Atta Ullah Noor <151811576+Noor27266@users.noreply.github.com>
2025-11-27 21:14:46 +0800

    Update RC Wall GUI Codes.py

--- RC Wall GUI Codes.py
+++ RC Wall GUI Codes.py
@@ -312,17 +312,7 @@ css(
 """
 )
 
-# ⬆️ move ONLY the inner right controls column up (relative to the DI–θ plot)
-css(
-    """
-<style>
-/* parent column  →  inner columns; last inner column is the controls */
-[data-testid="column"] [data-testid="column"]:last-child {
-    margin-top: -150px !important;   /* make more negative if you want higher */
-}
-</style>
-"""
-)
+
 
 
 # =============================================================================
@@ -820,19 +810,7 @@ with right:
 
 css("""
 <style>
-/* Move the whole controls stack:
-   - Up (translateY)
-   - Right (translateX)
-*/
-div[data-testid="stSelectbox"],
-div.stButton,
-div[data-testid="stDownloadButton"],
-.prediction-with-color {
-    transform: translate(40px, -150px);   /* (X , Y) */
-    /* X = right/left, Y = up/down */
-}
-</style>
-""")
+
 
 
 # =============================================================================
@@ -1271,6 +1249,7 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
 
 
 
